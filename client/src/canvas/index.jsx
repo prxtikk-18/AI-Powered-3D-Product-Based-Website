a027304fc4074840dbcbece9c9777999678ed6c1
Pratik <pratikrgws1234@gmail.com>
2023-06-17 09:57:34 +0530

    environment preset added

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -15,6 +15,7 @@ const CanvasModel = () => {
     >
       <ambientLight intensity={0.5} />
       <Environment path="/hdri/" files="potsdamer_platz_1k.hdr" />
+      <Environment preset="city" />
 
       <CameraRig>
         <Backdrop />
